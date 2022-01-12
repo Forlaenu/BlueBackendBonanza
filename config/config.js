fcae781a54dbb28acee02b4c02cd8a5cd1c810e6
Forlaenu <93723560+Forlaenu@users.noreply.github.com>
2022-01-12 15:49:04 -0600

    npm dotenv, sequelize init

---
+++ config/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "development": {
+    "username": process.env.DB_USERNAME || "postgres",
+    "password": process.env.DB_PASSWORD || null,
+    "database": process.env.DB_DATABASE || "backendProject",
+    "host": process.env.DB_HOST || '127.0.0.1',
+    "dialect": "postgres"
+  },
+  "production": {
+    "use_env_variable": "DATABASE_URL"
+  }
+}
