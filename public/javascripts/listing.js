cea16184a18450b3b6194fde7f6d0688b6b58417
Mayra Estrella <93684336+mayra84@users.noreply.github>
2022-01-17 19:07:58 -0600

    made user listing page

--- public/javascripts/listing.js
+++ public/javascripts/listing.js
@@ -6,6 +6,31 @@ listingForm.addEventListener('submit', (e) => {
         username: document.querySelector('#username').value,
         password: document.querySelector('#password').value
     })
-  
+
 })
 
+function renderListings(listings) {
+    const html = listings.map(listing => {
+        return `<div>
+        <div>${listing.text}</div>
+        <div>${listing.stars}/5 stars</div>
+        <div>${listing.visitedAt}</div>
+      </div>`
+    }).join('')
+    document.querySelector('#listings').innerHTML = html
+}
+
+function renderMovies(movies) {
+    const movieHtmlArray = movies.map(function (currentMovie) {
+        return `<div class="movie col-4">
+        <img src="${currentMovie.Poster}"<br/>
+        <h2>${currentMovie.Title}</h2>
+        <time datetime="\`0001\`">${currentMovie.Year}</time><br>
+        <button class="add-button" data-imdbid="${currentMovie.imdbID}">Add Me!</button><br/>
+        </div>
+        `
+    });
+
+    results = document.querySelector("#results");
+    results.innerHTML = movieHtmlArray.join('')
+};
